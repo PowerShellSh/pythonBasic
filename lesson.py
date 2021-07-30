9e10613273b2eb20be8ca7f1688fbb27334c4a53
小坂修吾郎 <shugorokosaka@kosakashuugorounoMacBook-Air.local>
2021-07-31 06:28:58 +0900

    first commit

---
+++ lesson.py
@@ -0,0 +1,11 @@
+# import lesson_package.utils
+#utilsファイルから
+# from lesson_package import utils
+
+#関数だけ(余り好まれない)
+
+from lesson_package.talk import human
+
+print(human.sing())
+print(human.cry())
+
